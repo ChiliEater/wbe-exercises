e47663e08c2626dcfa795867482004708ef4d884
Jonas <contact@jonascosta.ch>
2022-11-17 14:58:45 +0100

    Interaction works

---
+++ www/connect4/main.js
@@ -0,0 +1,66 @@
+import * as model from "./model.js"
+
+export const config = {
+    cell_dim: 100,
+    pad: 20,
+    rows: 7,
+    columns: 7,
+    placement_elem: "free",
+    puck_disabled: "disabled",
+    puck_class: "puck",
+    red: "red",
+    blue: "blue"
+}
+
+export let currentPuck
+let insertCells
+
+
+window.addEventListener("load", () => {
+    config.placement_elem = document.getElementById(config.placement_elem)
+    insertCells = Array.from(document.querySelectorAll("#insert-row > *"))
+    model.reset()
+
+    enablePreview()
+})
+
+export function setCurrentPuck(puck) {
+    currentPuck = puck
+}
+
+
+/**
+ * Displays a hovering animation at the apporpriate position
+ * @param {Event} event The event
+ */
+function hoverCell(column) {
+    return (event) => {
+        currentPuck.place(0, column)
+    }
+}
+
+function clickCell(column) {
+    return (event) => {
+        model.insertAt(column)
+
+    }
+}
+
+function enablePreview() {
+    insertCells.forEach((cell, i) => {
+        let hover = hoverCell(i)
+        cell.hover = hover
+        cell.addEventListener("pointerenter", hover)
+        let click = clickCell(i) 
+        cell.click = click
+        cell.addEventListener("pointerdown", click)
+        console.log(cell.hover)
+    });
+}
+
+function disablePreview() {
+    insertCells.forEach(cell => {
+        cell.removeEventListener("pointerenter", cell.hover)
+        cell.removeEventListener("pointerdown", cell.click)
+    })
+}
---
+++ www/connect4/model.js
@@ -0,0 +1,123 @@
+"use strict"
+import * as main from "./main.js"
+
+class Puck {
+
+    /**
+     * @type {string}
+     */
+    #color
+
+    /**
+     * @type {number}
+     */
+    #row
+
+    /**
+     * @type {number}
+     */
+    #column
+
+    /**
+     * @type {HTMLElement}
+     */
+    #element
+
+    constructor(color, row, column) {
+        this.#element = document.createElement("div")
+        this.#color = color
+        this.#row = row
+        this.#column = column
+
+        this.element.classList.add(main.config.puck_class, color, main.config.puck_disabled)
+        main.config.placement_elem.appendChild(this.element)
+    }
+
+    place(row, column) {
+        this.element.style.marginTop = `${row * main.config.cell_dim + main.config.pad / 2}px`
+        this.element.style.marginLeft = `${column * main.config.cell_dim + main.config.pad / 2}px`
+    }
+
+    show() {
+        this.element.classList.remove(main.config.puck_disabled)
+    }
+
+    get color(){
+        return this.#color
+    }
+
+    get row(){
+        return this.#row
+    }
+
+    get column(){
+        return this.#column
+    }
+
+    get element(){
+        return this.#element
+    }
+}
+
+export let turn = 0
+let board
+
+export function reset() {
+    board = Array(main.config.rows)
+    for (let i = 0; i < board.length; i++) {
+        board[i] = Array(main.config.columns)
+    }
+
+    main.config.placement_elem.innerHTML = ""
+    turn = 0
+    main.setCurrentPuck(new Puck(getColor(), 0, 0))
+    board[0][0] = main.currentPuck
+    console.log(board)
+    board[0][0].show()
+}
+
+/**
+ * Inserts the current puck at the given column and prepares the next turn
+ * @param {Number} column The column
+ */
+export function insertAt(column) {
+    let row = findFreeRow(column)
+    let puck = board[0][0]
+    board[row][column] = puck
+    puck.place(row, column)
+
+    if (checkWinner()) {
+        return
+    }
+    turn++
+    main.setCurrentPuck(new Puck(getColor(), 0, 0))
+    board[0][0] = main.currentPuck
+    main.currentPuck.show()
+
+    console.log(board)
+}
+
+/**
+ * Returns the correct color based on the current turn
+ * @returns A color
+ */
+function getColor() {
+    return turn % 2 == 0 ? "red" : "blue"
+}
+
+/**
+ * Searches the given column and returns the first empty row from the bottom
+ * @param {Number} column The column that should be searched
+ * @returns The empty row
+ */
+function findFreeRow(column) {
+    for (let i = main.config.rows - 1; i > -1; i--) {
+        if (board[i][column] === undefined) {
+            return i
+        }
+    }
+}
+
+function checkWinner() {
+    return false
+}
